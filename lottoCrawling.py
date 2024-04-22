227d8121729bf3ba51f0503bd804d65814e09d9b
JumpingDragon <bori555@ewhain.net>
2024-04-22 11:45:01 +0900

    로또 당첨 번호 가져오기

--- lottoCrawling.py
+++ lottoCrawling.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
 
-def get_lottoNum():
-    url = "https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo=1116"
+def get_lottoNum(count):  # 로또 추첨 회차를 입력받음
+    url = f"https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo={count}"
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
     # 로또 추첨일
@@ -18,3 +18,34 @@ def get_lottoNum():
     # 보너스 번호 반환
     bonusNum = int(soup.find('div', {'class': 'num bonus'}).find('p').text.strip())
 
+    lottoDic = {'lottoDate': lottoDate, 'lottoNumber': lottoNumList, 'BonusNumber': bonusNum}
+
+    return lottoDic
+
+
+lottoDf_list = []
+
+for cnt in range(1,1117):
+    lottoResult = get_lottoNum(cnt)
+
+    lottoDf_list.append({
+        'count': cnt,  # 로또 추첨 회차
+        'lottoDate': lottoResult['lottoDate'],  # 로또 추첨일
+        'lottoNum1': lottoResult['lottoNumber'][0],  # 로또 당첨 번호 중 첫번째 번호
+        'lottoNum2': lottoResult['lottoNumber'][1],  # 로또 당첨 번호 중 두번째 번호
+        'lottoNum3': lottoResult['lottoNumber'][2],  # 로또 당첨 번호 중 세번째 번호
+        'lottoNum4': lottoResult['lottoNumber'][3],  # 로또 당첨 번호 중 네번째 번호
+        'lottoNum5': lottoResult['lottoNumber'][4],  # 로또 당첨 번호 중 다섯번째 번호
+        'lottoNum6': lottoResult['lottoNumber'][5],  # 로또 당첨 번호 중 여섯번째 번호
+        'bonusNum': lottoResult['BonusNumber']  # 로또 보너스 번호
+    })
+    print(f"{cnt} 회 처리 중...")
+
+# print(lottoDf_list)
+
+lottoDF = pd.DataFrame(data=lottoDf_list, columns=['count', 'lottoDate', 'lottoNum1',
+                                         'lottoNum2', 'lottoNum3', 'lottoNum4',
+                                         'lottoNum4', 'lottoNum5', 'lottoNum6',
+                                         'bonusNum'])
+
+print(lottoDF)
